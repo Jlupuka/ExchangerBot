883a67c09b0a29a98c3910d7a47f38cbc20375d4
Jlupuka <olexiyfoma@yandex.ru>
2024-02-01 19:19:33 +0500

    add Percent column

--- databaseAPI/tables/walletAddressTable.py
+++ databaseAPI/tables/walletAddressTable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from databaseAPI.database import Base
 
 
@@ -8,3 +8,4 @@ class WalletAddress(Base):
     Id = Column(Integer, primary_key=True)
     NameNet = Column(String(255), nullable=False)
     Address = Column(String(255), unique=True, nullable=False)
+    Percent = Column(Float, default=5.0)
