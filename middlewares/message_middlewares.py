129b5314891d91965293350e7bbd9fa5c576cecf
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-03 00:01:33 +0500

    rename middlewar files and bugs fixes

---
+++ middlewares/message_middlewares.py
@@ -0,0 +1,28 @@
+from typing import Callable, Dict, Any, Awaitable
+
+from services import logger
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+from databaseAPI.commands.userCommands.user_commands import check_user
+from databaseAPI.commands.userCommands.admin_commands import make_admin, check_admin
+
+from config.config import load_config, Config
+
+config: Config = load_config()
+
+
+class DataBaseCheckUserMiddleware(BaseMiddleware):
+    def __init__(self):
+        super().__init__()
+
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any]
+    ) -> Any:
+        await check_user(user_id := event.from_user.id)
+        logger.info(f'User id={user_id}')
+        return await handler(event, data)
