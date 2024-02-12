b6502e80d534871cafb95e7ac0ee3a82718f1210
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-12 22:52:56 +0500

    Added an exception for a bad request

--- CastomExceptions/castomException.py
+++ CastomExceptions/castomException.py
@@ -1,10 +1,22 @@
 class NotFoundCryptoToken(Exception):
     """
-    Кастомное исключение.
-    Вызывается, если в системе нет такой крипто-сети
+    Called if there is no such crypto-network in the system
     """
+
     def __init__(self, *args: str | None) -> None:
         self.message = args[0] if args else None
 
     def __str__(self) -> str:
-        return f'{self.message}'
+        return self.message
+
+
+class BadRequest(Exception):
+    """
+    Called if bad response from API request
+    """
+
+    def __init__(self, *args: str | None) -> None:
+        self.message = args[0] if args else None
+
+    def __str__(self) -> str:
+        return self.message
