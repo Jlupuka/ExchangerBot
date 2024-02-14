5b484803f29421fedad6e27d557d7c247a4ccafa
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-14 22:04:08 +0500

    Added settings menu handler

--- handlers/callback_handlers/a_user_callback/admin_callback.py
+++ handlers/callback_handlers/a_user_callback/admin_callback.py
@@ -2,8 +2,11 @@ from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 
+from databaseAPI.commands.userCommands.admin_commands import get_workType, count_adminsWork, update_workType
+from databaseAPI.commands.walletAddress_commands import count_wallets
+from databaseAPI.commands.submissions_commands import get_all_missions_wait
 from filters.filters import IsAdmin
-from lexicon.lexicon import botMessages, startCallbackAdmin
+from lexicon.lexicon import botMessages, startCallbackAdmin, settingsMenu, workType, missions
 
 from keyboard.keyboard_factory import create_fac_menu
 from factories.factory import AdminCallbackFactory
@@ -34,3 +37,30 @@ async def statistics_handler(callback: CallbackQuery, callback_data: AdminCallba
                                      reply_markup=await create_fac_menu(AdminCallbackFactory,
                                                                         back=callback_data.back_page))
 
+
+@router.callback_query(AdminCallbackFactory.filter(F.page.in_({'settings', 'workType'})), IsAdmin())
+async def settings_handler(callback: CallbackQuery, callback_data: AdminCallbackFactory) -> None:
+    admin_work_type: bool = await get_workType(user_id=callback.from_user.id)
+    if callback_data.page == 'workType':
+        admin_work_type = await update_workType(user_id=callback.from_user.id, work_type=admin_work_type)
+    count_wait_missions: int = len(await get_all_missions_wait(status='WAIT'))
+    count_wallet: int = await count_wallets()
+    count_admin_work: int = await count_adminsWork()
+    buttons: dict[str: str] = settingsMenu.copy()
+    buttons['workType'] = workType[not admin_work_type]
+    if admin_work_type:
+        buttons.update(**missions)
+    await callback.message.edit_text(text=botMessages['settingsText'].format(workType=workType[admin_work_type],
+                                                                             countMissions=count_wait_missions,
+                                                                             countWallets=count_wallet,
+                                                                             adminsWork=count_admin_work),
+                                     reply_markup=await create_fac_menu(AdminCallbackFactory,
+                                                                        back_page=callback_data.page,
+                                                                        back='main',
+                                                                        sizes=(1, 2, 1) if admin_work_type else (2, 1),
+                                                                        **buttons))
+
+
+@router.callback_query(AdminCallbackFactory.filter(F.page == 'wallets'), IsAdmin())
+async def wallets_handler(callback: CallbackQuery, callback_data: AdminCallbackFactory) -> None:
+    pass
