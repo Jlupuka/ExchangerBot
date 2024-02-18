14e36e62f83a0c8444fdb6fd0ef6ae447622ebec
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-18 22:37:48 +0500

    added methods for wallet

--- services/wallets_service.py
+++ services/wallets_service.py
@@ -3,15 +3,26 @@ from databaseAPI.tables import WalletAddress
 
 
 async def get_wallets() -> dict[str: str]:
-    wallets_data: list[WalletAddress] = await get_all_wallets()
+    wallets_data: set[str] = await get_all_wallets()
     return {wallet.lower(): wallet.upper() for wallet in wallets_data}
 
 
-async def get_size_wallet(len_wallet: int) -> tuple[int, ...]:
+async def get_size_wallet(len_wallet: int, count_any_button: int = 0) -> tuple[int, ...]:
     result_wallet_size = list()
     for index in range(len_wallet):
         if index % 3 == 1:
             result_wallet_size.append(2)
         elif index % 3 == 2:
             result_wallet_size.append(1)
-    return tuple(result_wallet_size + [1, 1])
+    return tuple(result_wallet_size + [1] * count_any_button)
+
+
+async def preprocess_wallets(wallets: list[tuple[int, str]]) -> tuple[str, dict[str: str]]:
+    result_text = str()
+    result_dict = dict()
+    line_feed = '\n'
+    for index in range(len_wallets := len(wallets)):
+        result_text += (f'{wallets[index][0]}. <code>{wallets[index][1]}'
+                        f'</code>{"" if index == len_wallets - 1 else line_feed}')
+        result_dict[f'id-{wallets[index][0]}'] = str(wallets[index][0])
+    return result_text, {address_id: result_dict[address_id] for address_id in sorted(result_dict)}
