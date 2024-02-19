fa42bbe5c4ec4c21f41f02d31354976400d3d159
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-20 00:26:20 +0500

    Added update methods and fixed bugs

--- databaseAPI/commands/walletAddress_commands.py
+++ databaseAPI/commands/walletAddress_commands.py
@@ -1,7 +1,7 @@
 from services import logger
 
-from sqlalchemy import func, select, delete
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import func, select, delete, update
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.engine.result import ChunkedIteratorResult
 
 from databaseAPI.database import get_session
@@ -17,7 +17,7 @@ async def count_wallets() -> int:
         return count
 
 
-async def get_all_wallets() -> set[str]:
+async def get_all_name_net_wallets() -> set[str]:
     async with get_session() as session:
         sql: str = select(WalletAddress.NameNet)
         nets_chunk: ChunkedIteratorResult = await session.execute(sql)
@@ -58,6 +58,52 @@ async def delete_wallet(wallet_id: int) -> None:
     async with get_session() as session:
         sql: str = delete(WalletAddress).where(
             WalletAddress.Id == wallet_id
+        )
+        try:
+            await session.execute(sql)
+            await session.commit()
+            return None
+        except IntegrityError as IE:
+            logger.error(f"Indentation error in function '{__name__}': {IE}")
+            await session.rollback()
+
+
+async def get_wallet_data(wallet_id: int, get_data: str = None) -> float | WalletAddress:
+    async with get_session() as session:
+        sql: str = select(WalletAddress).where(
+            WalletAddress.Id == wallet_id
+        )
+        wallet_chunk: ChunkedIteratorResult = await session.execute(sql)
+        wallet: WalletAddress = wallet_chunk.scalars().one()
+        try:
+            return getattr(wallet, get_data) if get_data else wallet
+        except NoResultFound as NRF:
+            logger.error(f"Indentation error in function '{__name__}': {NRF}")
+            await session.rollback()
+
+
+async def update_work_type_wallet(wallet_id: int, work_type: bool) -> bool:
+    async with get_session() as session:
+        sql: str = update(WalletAddress).where(
+            WalletAddress.Id == wallet_id
+        ).values(
+            Status=work_type
+        )
+        try:
+            await session.execute(sql)
+            await session.commit()
+            return work_type
+        except IntegrityError as IE:
+            logger.error(f"Indentation error in function '{__name__}': {IE}")
+            await session.rollback()
+
+
+async def update_percent(wallet_id: int, percent: float) -> None:
+    async with get_session() as session:
+        sql: str = update(WalletAddress).where(
+            WalletAddress.Id == wallet_id
+        ).values(
+            Percent=percent
         )
         try:
             await session.execute(sql)
