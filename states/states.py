0f534401b19da0a56ae6672e095d559575676557
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-26 00:43:11 +0500

    Added states

--- states/states.py
+++ states/states.py
@@ -2,25 +2,30 @@ from aiogram.filters.state import State, StatesGroup
 
 
 class FSMFiatCrypto(StatesGroup):
-    crypto_to = State()
+    currency_to = State()
     requisites = State()
     check_validate = State()
+    check_validate_sum = State()
     method = State()
+    get_sum = State()
+    money_sent = State()
 
 
 class FSMCryptoFiat(StatesGroup):
     crypto_from = State()
     requisites = State()
+    money_sent = State()
 
 
 class FSMCryptoCrypto(StatesGroup):
     crypto_from = State()
     crypto_to = State()
     requisites = State()
+    money_sent = State()
 
 
 class FSMAddWallet(StatesGroup):
-    crypto_to = State()
+    currency_to = State()
     address = State()
     type_wallet = State()
     check_correct = State()
