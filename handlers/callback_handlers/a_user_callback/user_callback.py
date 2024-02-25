85f573210e421501289b2575526310b6db549a3c
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-26 00:51:39 +0500

    Fully realized exchange from RUB to CRYPTO

--- handlers/callback_handlers/a_user_callback/user_callback.py
+++ handlers/callback_handlers/a_user_callback/user_callback.py
@@ -3,8 +3,13 @@ from aiogram.filters import StateFilter
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
 
-from lexicon.lexicon import botMessages, startCallbackUser, profileUser, listMissionsUser, choiceToken, choiceMethod, \
-    backLexicon
+from databaseAPI.commands.submissions_commands import add_application
+from databaseAPI.commands.userCommands.user_commands import select_user
+from databaseAPI.commands.walletAddress_commands import get_all_name_net_crypto_wallets
+from databaseAPI.tables import WalletAddress, Submissions, Users
+from filters.filters import IsToken
+from lexicon.lexicon import botMessages, startCallbackUser, profileUser, listMissionsUser, choiceMethod, \
+    backLexicon, minSum, receiptVerification, getSum
 
 from keyboard.keyboard_factory import create_fac_menu
 from factories.factory import UserCallbackFactory
@@ -12,7 +17,9 @@ from factories.factory import UserCallbackFactory
 from magic_filter import F
 
 from services import logger
-from states.states import FSMFiatCrypto
+from services.cryptoService import CryptoCheck, min_sum
+from services.walletService import get_wallets, get_size_wallet, random_wallet
+from states.states import FSMFiatCrypto, FSMCryptoFiat, FSMCryptoCrypto
 
 router: Router = Router()
 
@@ -100,18 +107,21 @@ async def waiting_missions_handler(callback: CallbackQuery, callback_data: UserC
 @router.callback_query(UserCallbackFactory.filter(F.page == 'rub-crypto'))
 async def choice_rub_crypto(callback: CallbackQuery, callback_data: UserCallbackFactory, state: FSMContext) -> None:
     await state.clear()
-    await state.set_state(FSMFiatCrypto.crypto_to)
+    await state.set_state(FSMFiatCrypto.currency_to)
+    wallets_dict: dict[str: str] = await get_wallets(func=get_all_name_net_crypto_wallets)
+    size: tuple[int, ...] = await get_size_wallet(len_wallet=len(wallets_dict), count_any_button=1)
     await callback.message.edit_text(text=botMessages['choiceToken'],
                                      reply_markup=await create_fac_menu(UserCallbackFactory,
                                                                         back_page=callback_data.page,
                                                                         back=callback_data.back_page,
-                                                                        **choiceToken))
+                                                                        sizes=size,
+                                                                        **wallets_dict))
 
 
-@router.callback_query(UserCallbackFactory.filter(F.page.in_({'eth', 'btc'})),
-                       StateFilter(FSMFiatCrypto.crypto_to))
+@router.callback_query(UserCallbackFactory.filter(), IsToken(UserCallbackFactory),
+                       StateFilter(FSMFiatCrypto.currency_to))
 async def get_wallet(callback: CallbackQuery, callback_data: UserCallbackFactory, state: FSMContext) -> None:
-    await state.update_data(crypto_to=callback_data.page)
+    await state.update_data(currency_to=callback_data.page.upper())
     logger.debug(await state.get_data())
     await state.set_state(FSMFiatCrypto.requisites)
     await callback.message.edit_text(text=botMessages['getAddressCrypto'],
@@ -131,7 +141,8 @@ async def repeat_get_wallet(callback: CallbackQuery, state: FSMContext) -> None:
                                                                         back_page='main'))
 
 
-@router.callback_query(UserCallbackFactory.filter(F.page == 'yes'), StateFilter(FSMFiatCrypto.check_validate))
+@router.callback_query(UserCallbackFactory.filter(F.page == 'yes'),
+                       StateFilter(FSMFiatCrypto.check_validate, FSMFiatCrypto.get_sum))
 async def choice_method_get_sum(callback: CallbackQuery, callback_data: UserCallbackFactory, state: FSMContext) -> None:
     await state.set_state(FSMFiatCrypto.method)
     await callback.message.edit_text(text=botMessages['choiceMethod'],
@@ -142,5 +153,113 @@ async def choice_method_get_sum(callback: CallbackQuery, callback_data: UserCall
                                                                         back_name=backLexicon['cancelLexicon'],
                                                                         **choiceMethod))
 
-# TODO: дальше смысла пользовательскую часть писать нет. надо написать админскую часть,
-#  где можно добавлять и редактировать валюту и ее процент
+
+@router.callback_query(UserCallbackFactory.filter(F.page.in_({'rub', 'usd', 'repeatGetSum'})),
+                       StateFilter(FSMFiatCrypto.method, FSMFiatCrypto.check_validate_sum))
+async def rub_usd_method(callback: CallbackQuery, callback_data: UserCallbackFactory, state: FSMContext) -> None:
+    if callback_data.page != 'repeatGetSum':
+        await state.update_data(currency_from=callback_data.page.upper())
+    await state.set_state(FSMFiatCrypto.get_sum)
+    state_data: dict[str: str] = await state.get_data()
+    minimal_amount = min_sum
+    work_wallet: WalletAddress = await random_wallet(name_net='RUB')
+    if state_data['currency_from'] != 'RUB':
+        minimal_amount = await CryptoCheck.minimal_summa(minSum=min_sum,
+                                                         currency_to=state_data['currency_to'],
+                                                         currency_from=state_data['currency_from'])
+    currency_rate: float = await CryptoCheck.currency_rate(currency_from=state_data['currency_from'],
+                                                           currency_to=state_data['currency_to'],
+                                                           margins=work_wallet.Percent)
+    await state.update_data(work_walletRequisites=work_wallet.Address,
+                            walletPercent=work_wallet.Percent,
+                            walletId=work_wallet.Id)
+    await callback.message.edit_text(text=botMessages['getSum'].format(min_sum=minimal_amount,
+                                                                       currency_from=state_data['currency_from'],
+                                                                       currency_to=state_data['currency_to'],
+                                                                       currency_rate=currency_rate),
+                                     reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                                        back='yes',
+                                                                        **minSum))
+
+
+@router.callback_query(UserCallbackFactory.filter(F.page == 'minSum'), StateFilter(FSMFiatCrypto.get_sum))
+async def check_the_correct_transaction(callback: CallbackQuery, state: FSMContext) -> None:
+    state_data: dict[str: str] = await state.get_data()
+    await state.set_state(FSMFiatCrypto.check_validate_sum)
+    minimal_amount = await CryptoCheck.minimal_summa(minSum=min_sum,
+                                                     currency_to=state_data['currency_to'],
+                                                     currency_from=state_data['currency_from'])
+    await state.update_data(amount_from=minimal_amount)
+    walletPercent = state_data['walletPercent']
+    amount_to: float = await CryptoCheck.transaction_amount(amount=minimal_amount,
+                                                            currency_from=state_data['currency_from'],
+                                                            currency_to=state_data['currency_to'],
+                                                            margins=walletPercent)
+    await state.update_data(amount_to=amount_to)
+    text = botMessages['checkTheCorrectTransaction'].format(
+        name_net=state_data['currency_to'],
+        address=state_data['user_requisites'],
+        amount_from=minimal_amount,
+        currency_from=state_data['currency_from'],
+        currency_to=state_data['currency_to'],
+        amount_to=amount_to
+    )
+    await callback.message.edit_text(text=text,
+                                     reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                                        sizes=(2,),
+                                                                        back='main',
+                                                                        back_name=backLexicon['cancelLexicon'],
+                                                                        **getSum))
+
+
+@router.callback_query(UserCallbackFactory.filter(F.page == 'getSum'), StateFilter(FSMFiatCrypto.check_validate_sum))
+async def receipt_verification(callback: CallbackQuery, state: FSMContext) -> None:
+    await state.set_state(FSMFiatCrypto.money_sent)
+    state_data: dict[str: str] = await state.get_data()
+    rub_amount_from = await CryptoCheck.transaction_amount(amount=state_data['amount_from'],
+                                                           currency_to='RUB',
+                                                           currency_from=state_data['currency_from'])
+    text = botMessages['receiptVerification'].format(
+        amount_to=state_data['amount_to'],
+        currency_to=state_data['currency_to'].upper(),
+        amount_from=rub_amount_from,
+        type_transaction='RUB',
+        work_wallet=state_data['work_walletRequisites']
+    )
+    await callback.message.edit_text(text=text,
+                                     reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                                        back='main',
+                                                                        back_name=backLexicon['cancelLexicon'],
+                                                                        **receiptVerification))
+
+
+@router.callback_query(UserCallbackFactory.filter(F.page == 'sent'), StateFilter(FSMFiatCrypto.money_sent))
+async def create_mission(callback: CallbackQuery, state: FSMContext) -> None:
+    state_data: dict[str: str] = await state.get_data()
+    currency_to: str = state_data['currency_to']
+    wallet_requisites: str = state_data['user_requisites']
+    wallet_id: int = state_data['walletId']
+    amount_to: float = state_data['amount_to']
+    user_obj: Users = await select_user(callback.from_user.id)
+    match await state.get_state():
+        case FSMFiatCrypto.money_sent:
+            type_trans = 'RUB/CRYPTO'
+        case FSMCryptoFiat.money_sent:
+            type_trans = 'CRYPTO/RUB'
+        case FSMCryptoCrypto.money_sent:
+            type_trans = 'CRYPTO/CRYPTO'
+    mission_obj: Submissions = await add_application(user_id=user_obj.Id,
+                                                     address_id=wallet_id,
+                                                     amount=amount_to,
+                                                     typeTrans=type_trans,
+                                                     address_user=wallet_requisites)
+    await callback.message.answer(text=botMessages['createMission'].format(
+        mission_id=mission_obj.Id,
+        user_requisites=wallet_requisites,
+        amount=amount_to,
+        currency_to=currency_to
+    ),
+        reply_markup=await create_fac_menu(UserCallbackFactory,
+                                           back='main',
+                                           back_name=backLexicon['backMainMenu']))
+    await state.clear()
--- handlers/message_handlers/a_user_message/user_message.py
+++ handlers/message_handlers/a_user_message/user_message.py
@@ -3,13 +3,16 @@ from aiogram.types import Message
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 
+from databaseAPI.tables import WalletAddress
 from factories.factory import UserCallbackFactory
-from filters.filters import IsCryptoAddress
+from filters.filters import IsCryptoAddress, IsDigit
 from keyboard.keyboard_factory import create_fac_menu
 
 from lexicon.lexicon import botMessages, startCallbackUser, checkCorrectAddress, repeatAddress, errorLexicon, \
-    backLexicon
+    backLexicon, checkData, getSum, repeatGetSum
 from services import logger
+from services.cryptoService import CryptoCheck
+from services.walletService import random_wallet
 from states.states import FSMFiatCrypto
 
 router = Router()
@@ -28,11 +31,11 @@ async def start_handler(message: Message, state: FSMContext) -> None:
 
 @router.message(StateFilter(FSMFiatCrypto.requisites), IsCryptoAddress())
 async def add_mission(message: Message, state: FSMContext) -> None:
-    await state.update_data(requisites=message.text)
+    await state.update_data(user_requisites=message.text)
     await state.set_state(FSMFiatCrypto.check_validate)
     state_data = await state.get_data()
     logger.debug(state_data)
-    await message.answer(text=botMessages['checkCorrectAddress'].format(net=state_data['crypto_to'],
+    await message.answer(text=botMessages['checkCorrectAddress'].format(net=state_data['currency_to'].upper(),
                                                                         wallet_address=message.text),
                          reply_markup=await create_fac_menu(UserCallbackFactory,
                                                             back='main',
@@ -47,10 +50,58 @@ async def error_address(message: Message, state: FSMContext) -> None:
     await state.set_state(FSMFiatCrypto.check_validate)
     state_data = await state.get_data()
     logger.debug(state_data)
-    await message.answer(text=errorLexicon['errorAddress'].format(net=state_data['crypto_to'],
+    await message.answer(text=errorLexicon['errorAddress'].format(net=state_data['currency_to'],
                                                                   wallet_address=message.text),
                          reply_markup=await create_fac_menu(UserCallbackFactory,
                                                             back='main',
                                                             back_name=backLexicon['cancelLexicon'],
                                                             **repeatAddress
                                                             ))
+
+
+@router.message(StateFilter(FSMFiatCrypto.get_sum), IsDigit(check_min=True))
+async def check_the_correct_transaction(message: Message, state: FSMContext) -> None:
+    await state.set_state(FSMFiatCrypto.check_validate_sum)
+    await state.update_data(amount_from=float(message.text))
+    state_data: dict[str: str] = await state.get_data()
+    walletPercent = state_data['walletPercent']
+    amount_to: float = await CryptoCheck.transaction_amount(amount=state_data['amount_from'],
+                                                            currency_from=state_data['currency_from'].upper(),
+                                                            currency_to=state_data['currency_to'].upper(),
+                                                            margins=walletPercent)
+    await state.update_data(amount_to=amount_to)
+    text = botMessages['checkTheCorrectTransaction'].format(
+        name_net=state_data['currency_to'].upper(),
+        address=state_data['user_requisites'],
+        amount_from=state_data['amount_from'],
+        currency_from=state_data['currency_from'].upper(),
+        currency_to=state_data['currency_to'].upper(),
+        amount_to=amount_to
+    )
+    await message.answer(text=text, reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                                       sizes=(2,),
+                                                                       back='main',
+                                                                       back_name=backLexicon['cancelLexicon'],
+                                                                       **getSum))
+
+
+@router.message(StateFilter(FSMFiatCrypto.get_sum), IsDigit(check_min=False))
+async def failed_the_minimum_test(message: Message, state: FSMContext) -> None:
+    await state.set_state(FSMFiatCrypto.check_validate_sum)
+    state_data: dict[str: str] = await state.get_data()
+    await message.answer(text=errorLexicon['getSum_minimal'].format(amount=message.text,
+                                                                    currency_from=state_data['currency_from']),
+                         reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                            back='main',
+                                                            back_name=backLexicon['cancelLexicon'],
+                                                            **repeatGetSum))
+
+
+@router.message(StateFilter(FSMFiatCrypto.get_sum))
+async def failed_the_digit_test(message: Message, state: FSMContext) -> None:
+    await state.set_state(FSMFiatCrypto.check_validate_sum)
+    await message.answer(text=errorLexicon['IsDigit'].format(digit=message.text),
+                         reply_markup=await create_fac_menu(UserCallbackFactory,
+                                                            back='main',
+                                                            back_name=backLexicon['cancelLexicon'],
+                                                            **repeatGetSum))
