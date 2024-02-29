ee4d7f1aee6be220500fa365ced86e7f4e465d52
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-01 01:02:11 +0500

    Fixed table. Added AmountFrom column

--- databaseAPI/tables/submissionsTable.py
+++ databaseAPI/tables/submissionsTable.py
@@ -9,7 +9,8 @@ class Submissions(Base):
     Id = Column(Integer, primary_key=True)
     UserId = Column(Integer, ForeignKey('users.Id'), nullable=ForeignKey)
     AddressId = Column(Integer, ForeignKey('walletaddress.Id'), nullable=ForeignKey)
-    Amount = Column(Float, nullable=False)
+    AmountTo = Column(Float, nullable=False)
+    AmountFrom = Column(Float, nullable=False)
     TypeTrans = Column(String(255), nullable=False)  # crypto-rub | rub-crypto | crypto-crypto
     AddressUser = Column(String(255), nullable=False)
     Status = Column(String(255), default='WAIT')  # WAIT | ACCEPTED | COMPLETED
