2a0b3dd754cb4aa47c83a8526af4208677fb43df
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-03 03:55:57 +0500

    Fixed bugs

--- factories/factory.py
+++ factories/factory.py
@@ -5,15 +5,15 @@ from typing import Optional
 class UserCallbackFactory(CallbackData, prefix='user'):
     page: Optional[str]
     back_page: Optional[str]
-    mission_page: Optional[int] = 0
 
 
 class AdminCallbackFactory(CallbackData, prefix='admin'):
     page: Optional[str]
     back_page: Optional[str]
+    mission_page: Optional[int] = 0
 
 
 class MissionCallbackFactory(CallbackData, prefix='mission'):
-    page: Optional[str]
-    back_page: Optional[str | int]
     mission_id: Optional[int]
+    page: Optional[str | int]
+    back_page: Optional[str | int]
