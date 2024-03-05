0b4bb76485fa4d8ce034d4799ddf8ffe1f9671c6
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-05 23:48:14 +0500

    Module for dynamic change of machine states

---
+++ services/stateService.py
@@ -0,0 +1,22 @@
+from aiogram.fsm.context import FSMContext
+
+from states.states import FSMFiatCrypto, FSMCryptoFiat, FSMCryptoCrypto
+
+
+class StateService:
+    @staticmethod
+    async def set_states(state_name: str, state_data: dict[str: str], state: FSMContext) -> None:
+        """
+        A function that sets the state for a particular FSM
+        :param state_name: (str) Name of the state to switch to
+        :param state_data: (dict[str: str) State data
+        :param state: (aiogram.fsm.context.FSMContext)
+        :return: None
+        """
+        match state_data['typeTransaction']:
+            case 'RUB-CRYPTO':
+                await state.set_state(getattr(FSMFiatCrypto, state_name))
+            case 'CRYPTO-RUB':
+                await state.set_state(getattr(FSMCryptoFiat, state_name))
+            case 'CRYPTO-CRYPTO':
+                await state.set_state(getattr(FSMCryptoCrypto, state_name))
