5e42ae3781031498f8c41ba13fe1f3fcfa06fb3b
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-10 16:24:52 +0500

    Added method to genereta verif_number and added method statistic_admin

--- services/userService.py
+++ services/userService.py
@@ -38,13 +38,59 @@ class UserService:
             favorite_category[mission.TypeTrans] = favorite_category.get(mission.TypeTrans, 0) + 1
             count_each_mission[mission.Status] = count_each_mission.get(mission.Status, 0) + 1
             if mission.Status == 'COMPLETED':
-                amount_from = await CryptoCheck.transaction_amount(amount=mission.AmountFrom,
-                                                                   currency_from=wallet.NameNet,
-                                                                   currency_to='RUB',
-                                                                   margins=1)
+                amount_from = (await CryptoCheck.transaction_amount(amount=mission.AmountFrom,
+                                                                    currency_from=wallet.NameNet,
+                                                                    currency_to='RUB',
+                                                                    margins=1)
+                               if wallet.NameNet != 'СПБ'
+                               else mission.AmountFrom)
                 total_amount += amount_from
         return {**count_each_mission,
                 'countTransaction': count_transaction,
                 'totalAmount': total_amount,
-                'averageAmount': round(total_amount / count_each_mission['COMPLETED'], 2),
+                'averageAmount': round(total_amount / count_each_mission['COMPLETED'], 2) if count_each_mission[
+                    'COMPLETED'] else 0,
                 'typeTransaction': max(favorite_category)}
+
+    @staticmethod
+    async def verif_number() -> int:
+        return random.randint(10 ** 5, 10 ** 6 - 1)
+
+    @staticmethod
+    async def statistic_admin() -> dict:
+        data: list[Row] = await SubmissionsAPI.data_completed_submissions()
+        result = {'gainToDay': 0,
+                  'gainToWeek': 0,
+                  'gainToMonth': 0,
+                  'gainTotal': 0,
+                  'maxExchangeAmount': 0,
+                  'maxExchangeUserID': 0,
+                  'maxExchangeDateTime': 0}
+        cache_currency: dict[str: int | float] = {'СПБ': 1, 'RUB': 1}
+        now = datetime.datetime.utcnow()
+        for mission, wallet, user in data:
+            amount_user = mission.AmountFrom
+            if cache_currency.get(wallet.NameNet) is None:
+                cache_currency[wallet.NameNet] = await CryptoCheck.currency_rate(currency_to=wallet.NameNet,
+                                                                                 currency_from='RUB',
+                                                                                 margins=1)
+            amount_user *= cache_currency[wallet.NameNet]
+            amount_admin = amount_user * (wallet.Percent - 1)
+            if mission.DateTime >= now - datetime.timedelta(days=1):
+                result['gainToDay'] += amount_admin
+            if mission.DateTime >= now - datetime.timedelta(weeks=1):
+                result['gainToWeek'] += amount_admin
+            if mission.DateTime >= now - datetime.timedelta(days=30):
+                result['gainToMonth'] += amount_admin
+            if result['maxExchangeAmount'] < amount_user:
+                result['maxExchangeAmount'] = amount_user
+                result['maxExchangeUserID'] = user.UserId
+                result['maxExchangeDateTime'] = mission.DateTime
+            result['gainTotal'] += amount_admin
+        result['gainToDay'] = round(result['gainToDay'], 2)
+        result['gainToWeek'] = round(result['gainToWeek'], 2)
+        result['gainToMonth'] = round(result['gainToMonth'], 2)
+        result['gainTotal'] = round(result['gainTotal'], 2)
+        result['maxExchangeDateTime'] = await UserService.format_date_string(
+            date_string=str(result['maxExchangeDateTime']))
+        return result
