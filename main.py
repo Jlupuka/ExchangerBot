3765677c09ed06c5048ca745a11893381935bbd4
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-11 01:34:37 +0500

    Fixed bugs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram.fsm.storage.redis import RedisStorage, Redis
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.methods import GetUpdates
 
-from databaseAPI.tables import *  # Для создания базы данных (да-да...)
+from databaseAPI.tables import *
 
 from services.service import LoadService
 
@@ -20,33 +20,28 @@ from services import logger
 
 
 async def main() -> None:
-    # Подгружаем конфиг
     config: Config = load_config()
 
     redis: Redis = Redis(host=config.Redis.HOST, port=config.Redis.PORT)
 
     storge: RedisStorage = RedisStorage(redis=redis)
 
-    # Создаем объекты бота и диспетчера
-    bot: Bot = Bot(token=config.TelegramBot.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot_properties: DefaultBotProperties = DefaultBotProperties(parse_mode=ParseMode.HTML)
+
+    bot: Bot = Bot(token=config.TelegramBot.TOKEN, default=bot_properties)
     dp: Dispatcher = Dispatcher(storage=storge)
 
-    # Создаем базу данных
     await create_base()
 
-    # # Создаем копки menu
     await load_main_command(logger=logger, bot=bot)
 
-    # Подключаем роутеры
     await LoadService.load_router(dp=dp)
 
-    # Подключаем мидлвари
     await LoadService.load_middlewares(dp=dp)
     bot.session.middleware(RequestLogging(ignore_methods=[GetUpdates]))
 
     logger.info('Starting bot')
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
