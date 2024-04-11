2f3056358ce60c3217c1c1f9b52677d7c7efd6a5
Jlupuka <dima.usachev.0790@mail.ru>
2024-04-11 22:00:11 +0500

    Added error handling when deleting a wallet

--- handlers/callback_handlers/a_user_callback/admin/mainMenu/wallets/delete_wallet.py
+++ handlers/callback_handlers/a_user_callback/admin/mainMenu/wallets/delete_wallet.py
@@ -6,11 +6,7 @@ from aiogram.types import CallbackQuery
 
 from databaseAPI.commands.walletAddress_commands import WalletAPI
 from filters.filters import IsAdmin
-from lexicon.lexicon import (
-    botMessages,
-    sureLexicon,
-    addressDelete,
-)
+from lexicon.lexicon import botMessages, sureLexicon, addressDelete, errorLexicon
 
 from keyboard.keyboard_factory import Factories
 from factories.factory import AdminCallbackFactory
@@ -40,12 +36,21 @@ async def delete_address_menu(
 async def delete_address(callback: CallbackQuery, state: FSMContext) -> None:
     state_data = await state.get_data()
     address_id = int(state_data["token"].split("-")[1])
-    await WalletAPI.delete_wallet(wallet_id=address_id)
-    await callback.message.edit_text(
-        text=botMessages["deleteAddress"].format(
-            wallet=state_data[state_data["token"]]
-        ),
-        reply_markup=await Factories.create_fac_menu(
-            AdminCallbackFactory, **addressDelete
-        ),
-    )
+    if await WalletAPI.delete_wallet(wallet_id=address_id):
+        await callback.message.edit_text(
+            text=botMessages["deleteWallet"].format(
+                wallet=state_data[state_data["token"]]
+            ),
+            reply_markup=await Factories.create_fac_menu(
+                AdminCallbackFactory, **addressDelete
+            ),
+        )
+    else:
+        callback.message.edit_text(
+            text=errorLexicon["deleteWallet"].format(
+                wallet=state_data[state_data["token"]]
+            ),
+            reply_markup=await Factories.create_fac_menu(
+                AdminCallbackFactory, **addressDelete
+            ),
+        )
